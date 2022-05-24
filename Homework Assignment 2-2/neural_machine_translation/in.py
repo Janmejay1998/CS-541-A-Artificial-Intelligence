9c6e43bd147c2cfec0072f6a27b46cf6926031e0
Janmejay1998 <33492805+Janmejay1998@users.noreply.github.com>
2022-05-23 21:25:29 -0400

    Add files via upload

---
+++ Homework Assignment 2-2/neural_machine_translation/in.py
@@ -0,0 +1,15 @@
+import torch
+import torchvision.models as models
+
+# Making the code device-agnostic
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+#device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+# Instantiating a pre-trained model
+#model = models.resnet18(pretrained=True)
+
+# Transferring the model to a CUDA enabled GPU
+#model = model.to(device)
+
+# Now the reader can continue the rest of the workflow
+# including training, cross validation, etc!
---
+++ Homework Assignment 2-2/readme.txt
@@ -0,0 +1,3 @@
+jmohanty_nyc.ipynb contains program for implementing the Uniform Cost Search and A* search algorithm Implementation.
+
+beam_search.ipynb contains program for Neural machine translation.
